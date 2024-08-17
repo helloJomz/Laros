cd94e063fdebf5d952db82f37d57399bb20706eb
Jomark <jomarkamante@gmail.com>
2024-08-17 15:16:54 +0800

    test again

--- server/dist/routes/user.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const UserController_1 = require("../controllers/UserController");
-const router = (0, express_1.Router)();
-router.post("/getuserbydisplayname", UserController_1.getUserByDisplayNameController);
-router.get("/getuser", UserController_1.getUserByIdController);
-exports.default = router;
