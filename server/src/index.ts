0fdccf58d19d4c26490edec9710e679bc8885580
Jomark <jomarkamante@gmail.com>
2024-08-17 17:46:47 +0800

    enabled cors for all

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,12 +17,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "https://laros-lime.vercel.app",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
